25afa4f185e0004cb14fc2ba74ff7d9728049286
vari <bryan_99yong@hotmail.com>
2024-02-08 20:01:07 -0500

    smirnof

--- src/Component/DisplaySection/DisplaySection.js
+++ src/Component/DisplaySection/DisplaySection.js
@@ -6,11 +6,11 @@ import { MdDoubleArrow } from "react-icons/md";
 import img1 from "../Assets/grocery/dp-img1.png"
 import img2 from "../Assets/grocery/dp-img2.jpg"
 import img3 from "../Assets/grocery/dp-img3.jpg"
-import img8 from "../Assets/grocery/flowerkolly.png"
+import img8 from "../Assets/huarapearimagenes/superoferta.png"
 import cherry from "../Assets/grocery/cherry.png"
 import tomato from "../Assets/grocery/tomato-slice.png"
 import orange from "../Assets/grocery/orange-slice.png"
-import plate from "../Assets/grocery/plate.png"
+import vodka from "../Assets/huarapearimagenes/smirnoftanaribdo.png"
 
 const DisplaySection = () => {
   return (
@@ -18,7 +18,7 @@ const DisplaySection = () => {
         <div className='flex gap-[51px]'>
             <div className='display-first-section'>
 
-                <img src={img8} className='absolute -z-10' alt=''/>
+                <img src={img8} className='absolute left-[230px] bottom-[100px] -z-2' alt='' style={{width: '200px'}}/>
 
                 <div className='absolute right-[40px] top-[20px]'>
                     <img src={cherry} alt=''/>
@@ -32,14 +32,15 @@ const DisplaySection = () => {
                     <img src={orange} alt=''/>
                 </div>
 
-                <div className='absolute left-[-150px] bottom-[-180px]'>
-                    <img src={plate} alt=''/>
+                <div className='absolute left-[-10px] bottom-[-20px]'>
+                    <img src={vodka} alt='' style={{width: '200px'}}/>
                 </div>
 
                 <div className='content-sec-one'>
-                    <h2>DELICIOSA</h2>
-                    <h1>ENSALADA</h1>
-                    <h3>EVERYDAY</h3>
+
+                    <h2>SMIRNOFF</h2>
+                    <h1>EXIGE LO MEJOR</h1>
+                    <h3>NO TE CONFORMES CON MENOS</h3>
 
                     <div className='shop-now-btn ml-[250px]'>
                         <button> Comprar Ahora</button>
